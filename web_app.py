2938c396ad5b315af0f05dc9cf804dacaeafaf4c
Shubham Ahire <shubhamahireofficial@gmail.com>
2026-07-05 16:38:50 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -247,3 +247,6 @@ def main():
     app.run(host=HOST, port=PORT, debug=False, threaded=True)
 
 if __name__ == "__main__": main()
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
